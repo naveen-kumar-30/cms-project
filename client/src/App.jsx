0de28028d36652ace74ec76e086b41a0fe3cbc92
naveen-kumar-30 <naveenkumarp3052004@gmail.com>
2025-05-28 17:39:56 +0530

    home-page-commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-function App() {
-  return (
-    
-  );
-}
+import { Route,createBrowserRouter,createRoutesFromElements,RouterProvider,} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
 
-export default App;
+ 
+
+const App = () => {
+
+ const router = createBrowserRouter(
+   createRoutesFromElements(
+     <Route path='/' element={<MainLayout/>}>
+     <Route index element={<HomePage/>} />
+     </Route>
+   )
+ );
+
+  return <RouterProvider router={router}/>;
+};
+
+export default App
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  server: {
+    port: 2000,
+    proxy: {
+      '/api' : {
+        target: 'http://localhost:9000',
+        changeOrigin : true,
+        rewrite: (path) => path.replace(/^\/api/,''),
+      },
+    }
+  }
+})
