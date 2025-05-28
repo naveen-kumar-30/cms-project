e93a2608c88e498ae20db1707b4ff29017c59148
naveen-kumar-30 <naveenkumarp3052004@gmail.com>
2025-05-28 19:36:16 +0530

    deleted-page-commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import { Route,createBrowserRouter,createRoutesFromElements,RouterProvider,} from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
+import JobPages from './pages/Jobpages';
+import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
+import Jobpage from './pages/JobPage';
 
- 
+
 
 const App = () => {
 
@@ -10,6 +15,11 @@ const App = () => {
    createRoutesFromElements(
      <Route path='/' element={<MainLayout/>}>
      <Route index element={<HomePage/>} />
+      <Route path='/jobs' element={<JobPages/>} />
+     <Route path='/add-job' element={<AddJobPage/>}  />
+      <Route path='/edit-job/:id' element={<EditJobPage/>}/>
+      <Route path='/jobs/:id' element={<Jobpage/>} />
+     <Route path='*' element={<NotFoundPage/>} />
      </Route>
    )
  );
